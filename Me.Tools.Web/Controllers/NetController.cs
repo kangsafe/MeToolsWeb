4ac0b07f94a3559e3fccf4dcd8cb473aee062f54
kangsafe <kangsafe@hotmail.com>
2016-07-24 17:09:31 +0800

    模拟Http

---
+++ Me.Tools.Web/Controllers/NetController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace Me.Tools.Web.Controllers
+{
+    public class NetController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult http()
+        {
+            return View();
+        }
+
+    }
+}
